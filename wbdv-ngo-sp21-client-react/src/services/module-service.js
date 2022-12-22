640172a2609052f055fe1f140fe982ae664f5c72
Trang <trangkch1997@gmail.com>
2022-12-22 15:13:00 -0500

    Create services for modules, lessons, and topics

---
+++ wbdv-ngo-sp21-client-react/src/services/module-service.js
@@ -0,0 +1,32 @@
+const API_URL = `https://wbdv-generic-server.herokuapp.com/api/ng.th`
+
+export const createModule = (courseID, module) =>
+    fetch(`${API_URL}/courses/${courseID}/modules`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(module)
+    }).then(response => response.json());
+
+export const findModulesForCourse = courseID =>
+    fetch(`${API_URL}/courses/${courseID}/modules`)
+        .then(response => response.json());
+
+export const findModule = moduleID => 
+    fetch(`${API_URL}/modules/${moduleID}`)
+        .then(response => response.json());
+
+export const updateModule = (moduleID, module) =>
+    fetch(`${API_URL}/modules/${moduleID}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(module)
+    }).then(response => response.json());
+
+export const deleteModule = moduleID =>
+    fetch(`${API_URL}/modules/${moduleID}`, {
+        method: 'DELETE'
+    }).then(response => response.json())
